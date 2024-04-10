e6171dc0c83f260a8fcd373fb21b49c11ad514af
MercierTanguy <tanguy.mercier@efrei.net>
2024-04-10 12:10:48 +0200

    Footer ajouté

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <Header></Header>
   <router-view></router-view>
+  <Footer></Footer>
 </template>
 <script setup>
 
 import Header from './components/Header.vue'
-
+import Footer from './components/Footer.vue'
 
 
 </script>
