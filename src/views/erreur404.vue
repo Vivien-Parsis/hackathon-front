78ab241851be897380078c0cfb95cbeb5da02622
Karlamilyi <adam.dauve@gmail.com>
2024-04-11 18:27:42 +0200

    multiple updates

---
+++ src/views/erreur404.vue
@@ -0,0 +1,14 @@
+<template>
+
+    <h1> ERREUR , AUCUNE PAGE TROUVER</h1>
+
+
+</template>
+
+<style>
+
+h1{
+    text-align: center;
+    margin: auto;
+}
+</style>
--- src/views/loginPageChoice.vue
+++ src/views/loginPageChoice.vue
@@ -1,5 +1,40 @@
 <template>
-
-    <h1> Login page choice</h1>
-
-</template>
+    <div class="link-container">
+      <a href="/loginAdmin" class="Admin-link">
+        <p>ESPACE ADMIN</p>
+      </a>
+      <a href="/loginUser" class="User-link">
+        <p>ESPACE VISITEUR</p>
+      </a>
+    </div>
+  </template>
+  
+  <style>
+  .link-container {
+    display: flex;
+    justify-content: space-around;
+    margin-top: 20px; 
+  }
+  
+  .Admin-link,
+  .User-link {
+    text-decoration: none;
+    color: #FFD39F;
+    background-color: #49290B; 
+    border: 4px solid #FFD39F; 
+    margin: 0 10px; 
+  }
+  
+  .Admin-link p,
+  .User-link p {
+    padding: 10px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 90px;
+  }
+  
+  p:hover {
+    box-shadow: inset 800px 0 0 0 #FFD39F;
+    color: #49290B;
+  }
+  </style>
